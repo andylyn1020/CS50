f07ff40a0fead96794bcee3b6c15f37ee0f9fc7a
andylyn1020 <162807498+andylyn1020@users.noreply.github.com>
2024-03-16 10:00:07 +0000

    Sat, Mar 16, 2024, 10:00 AM +00:00

--- phone.c
+++ phone.c
@@ -1,9 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string>
+#include <string.h>
 
-void main(void)
+int main(void)
 {
     string names[] = {"a", "b", "c"};
-    string number[] = {"1", "2", "3"};
+    string numbers[] = {"1", "2", "3"};
+
+    string name = get_string("Name: ");
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
