b5f7e435fc9d36c89514918432d4c65418018c9e
andylyn1020 <162807498+andylyn1020@users.noreply.github.com>
2024-03-17 02:59:45 +0000

    Sun, Mar 17, 2024, 2:59 AM +00:00

--- try.c
+++ try.c
@@ -13,13 +13,12 @@ void draw(int n)
 {
     if (n <= 0)
     return;
+    string t = # * n
 {
     draw (n-1);
 
     for (int i = 0; i < n; i++)
-
         {
-            string total = (# * n)
             printf("t");
         }
     }
